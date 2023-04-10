e01cc683bcc4256c7db20ff634c012d815c4dbc8
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-04-10 19:01:15 +0530

    Lint in python

---
+++ List.py
@@ -0,0 +1,13 @@
+friends=["Yash","Harsh","Rohan"]
+# length
+print(len(friends))
+
+# each element
+print(friends[2])
+print(friends[0])
+print(friends[-1])
+
+# conditions
+if 'Rohan' in friends:
+    print("Yes")
+
