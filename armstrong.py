7e872936ebe22df2bf815d401a0787d6782a52e4
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-04-10 19:01:41 +0530

    Armstrong Number

---
+++ armstrong.py
@@ -0,0 +1,13 @@
+
+# Armstrong Number
+num=int(input("Number: "))
+n=num
+sum=0;
+while(num>0):
+    rem=num%10;
+    sum=sum+(rem**3)
+    num=num//10;
+if(sum==n):
+    print(f"{n} is Armstrong Number")
+else:
+    print(f"{n} is not Armstrong Number")
