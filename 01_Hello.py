566ac48e25002c11a55ea331ffdb3b09f7515b7b
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-04-10 13:47:19 +0530

    First

---
+++ 01_Hello.py
@@ -0,0 +1,9 @@
+# Author--Yashodip Beldar
+# Date--6-09-2022
+'''
+  Learning python with Code With Harry
+  ****Thanks For Reading****
+'''
+#Above all is comments in Python
+import os #importing os module
+print("Hello Yashodip")
