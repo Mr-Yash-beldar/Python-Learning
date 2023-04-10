c2ffed728a557b268a939b046cd3c11056d232b7
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-04-10 19:04:00 +0530

    Python list methods

---
+++ list_methods.py
@@ -0,0 +1,21 @@
+friend=["Yeshu","Harshu","Dipya"]
+
+# list methods
+# insert at any index
+friend.insert(1,"Brothers")
+print(friend)
+
+# append add element at list add at last
+friend.append("Shipur")
+print(friend)
+
+# pop delete form last
+friend.pop()
+print(friend)
+
+# delete at particular index
+friend.pop(1)
+print(friend)
+
+# delete list
+# delete  friend[]
