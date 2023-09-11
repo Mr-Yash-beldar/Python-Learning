7637c8bcc6a92a7c472612727795a8a767b5db12
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-09-11 19:12:07 +0530

    sum of series version 2.0

---
+++ series2.py
@@ -0,0 +1,11 @@
+def fact(num)-> int:  
+    fact=1;
+    for i in range (1,num+1):
+        fact=fact*i
+    return fact
+x=2
+n=5
+sum=1
+for i in range(1,n+1):
+    sum=sum+(pow(x, i)/fact(i))
+print(sum)
