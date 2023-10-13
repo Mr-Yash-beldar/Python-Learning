5f1f9fcab9771e913f7b0ed2cbca513ec45dba2c
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-13 15:09:47 +0530

    file handling practical

---
+++ Practical/Module/FileHandling/filehanding.py
@@ -0,0 +1,19 @@
+# f=open("demo.txt","r")
+# print(f.read())
+
+path = 'C:/Users/yasho/OneDrive/Desktop/ADBMS Project/Stuff/useit.txt'
+
+# f=open(path,"r")
+# print(f.read())
+
+
+# f=open("demo.txt","r")
+# for x in f:
+#     print(x)
+
+# f=open(path,"a")
+# print(f.write("I am Yashodip Beldar"))
+
+f=open(path,"r")
+print(f.readLIne())
+print(f.readLIne())
