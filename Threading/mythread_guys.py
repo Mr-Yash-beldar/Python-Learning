1ecbcc5afdececdcb74e5bdd6286bd368e90350e
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-13 15:08:34 +0530

    Theading in python

---
+++ Threading/mythread_guys.py
@@ -0,0 +1,12 @@
+from threading import *
+
+def show():
+    for i in range(1,11):
+        print("Child Thread")
+
+t=Thread(target=show)
+t.start()
+for i in range (1,11):
+    print("Main Thread")
+
+
