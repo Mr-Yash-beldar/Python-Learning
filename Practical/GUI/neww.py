a19fc19b009c52c5364de991145fc339de77e08b
Yashodip Beldar <yashodip2003beldar@gmail.com>
2023-12-08 14:51:22 +0530

    GUI related added

---
+++ Practical/GUI/neww.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+hospital=tk.Tk()
+hospital.title(("Hospital Registration Form"))
+hospital.geometry("500x400")
+hospital.configure(bg="lightblue")
+tk.Label(hospital,).pack()
+hospital.mainloop()
